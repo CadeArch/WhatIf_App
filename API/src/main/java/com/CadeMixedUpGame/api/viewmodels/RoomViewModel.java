57961c07b3885ec558f7061eb4aa35da9de8b249
Cade Rasmussen <rascade@gmail.com>
2021-12-03 12:23:00 -0700

    removed ambiguous letters from room id names generation, added instructions to unlock things in profile page.

--- API/src/main/java/com/CadeMixedUpGame/api/viewmodels/RoomViewModel.java
+++ API/src/main/java/com/CadeMixedUpGame/api/viewmodels/RoomViewModel.java
@@ -27,7 +27,7 @@ public class RoomViewModel extends ViewModel {
     Room room;
     public ArrayList<String> roomNames = new ArrayList<>();
     public DatabaseReference db;
-    // removed I and l becuase they were ambiguous with the font i am using
+    // removed capitol I and lowercase l because they were ambiguous with the font i am using
     String allChars = "a b c d e f g h i j k m n o p q r s t u v w x y z A B C D E F G H J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9";
     String[] usableCharacter;
     public MutableLiveData<Boolean> inProgress = new MutableLiveData<Boolean>();
